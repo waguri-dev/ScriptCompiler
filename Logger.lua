1abf305476f3734db0acc39e9108f76339d05657
wagurii <161109688+waguri-dev@users.noreply.github.com>
2025-11-12 13:04:54 +0500

    Update Logger.lua

--- Logger.lua
+++ Logger.lua
@@ -15,15 +15,37 @@ _G[getLoader()] = function(n,c)
   if f then
     return f
   else
-    print("load error : " .. e)
+    print("load error -> " .. e)
     return nil
   end
+end
+
+function table.concat(tb, co)
+  if type(tb) ~= "table" then
+    print("table.concat -> argument[1]  is not a table.")
+    return
+  end
+  if type(co) ~= "string" then
+    print("table.concat -> argument[2] is not an string, but it is expected, so its handled")
+    co = co or ""
+  end
+  return _g.table.concat(tb,co)
+end
+
+
+function table.unpack(tb)
+  if type(tb) ~= "table" then
+    print("table.unpack -> argument[1]  is not a table.")
+    return
+  end
+  return _g.table.unpack(tb)
 end
 
 function string.char(...)
   local Args = {...}
   for _, val in pairs(Args) do
     if val > 255 then
+      print("string.char -> recv value over 255, reduce to 255")
       Args[_] = val % 256
     end
   end
